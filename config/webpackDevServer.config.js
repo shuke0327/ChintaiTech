fd53b92da5928d5945edf6a55206826ff3f9aee9
shuke0327 <jingkai64@gmail.com>
2020-09-04 21:07:49 +0800

    use for heroku: redirect ssl

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -6,11 +6,13 @@ const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMi
 const ignoredFiles = require('react-dev-utils/ignoredFiles');
 const paths = require('./paths');
 const fs = require('fs');
+const sslRedirect = require('heroku-ssl-redirect')
+
 
 const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
 const host = process.env.HOST || '0.0.0.0';
 
-module.exports = function(proxy, allowedHost) {
+module.exports = function (proxy, allowedHost) {
   return {
     // WebpackDevServer 2.4.3 introduced a security fix that prevents remote
     // websites from potentially accessing local content through DNS rebinding:
@@ -87,7 +89,8 @@ module.exports = function(proxy, allowedHost) {
         // This registers user provided middleware for proxy reasons
         require(paths.proxySetup)(app);
       }
-
+      // use ssl redirect for Heroku
+      app.use(sslRedirect());
       // This lets us fetch source contents from webpack for the error overlay
       app.use(evalSourceMapMiddleware(server));
       // This lets us open files from the runtime error overlay.
